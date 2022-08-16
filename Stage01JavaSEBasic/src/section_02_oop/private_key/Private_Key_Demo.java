93f0aa36848ff39410b029a64bff6bd869e0278b
qihao <qihao_97@126.com>
2022-08-16 23:33:39 +0800

    带有私有成员变量的学生类，要用set和get方法来设置和获取成员变量

---
+++ Stage01JavaSEBasic/src/section_02_oop/private_key/Private_Key_Demo.java
@@ -0,0 +1,19 @@
+package section_02_oop.private_key;
+
+/**
+ * @Author: Qihao
+ * @Time: 2022/2022/8/16/23:25
+ * @Descriptions: private关键字示例
+ */
+public class Private_Key_Demo {
+    public static void main(String[] args) {
+        StudentPrivate studentPrivate1 = new StudentPrivate();
+        System.out.println("name = " + studentPrivate1.getName() + "  age = " + studentPrivate1.getAge());
+
+        System.out.println("------------------------------");
+
+        studentPrivate1.setName("马尔扎哈");
+        studentPrivate1.setAge(25);
+        System.out.println("name = " + studentPrivate1.getName() + "  age = " + studentPrivate1.getAge());
+    }
+}
---
+++ Stage01JavaSEBasic/src/section_02_oop/private_key/StudentPrivate.java
@@ -0,0 +1,27 @@
+package section_02_oop.private_key;
+
+/**
+ * @Author: Qihao
+ * @Time: 2022/2022/8/16/23:25
+ * @Descriptions: 带private的Student类
+ */
+public class StudentPrivate {
+    private int age = 20;
+    private String name = "迪丽热巴";
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
