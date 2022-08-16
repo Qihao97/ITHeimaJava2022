53407653228e914862c412d882a40889f983ef54
qihao <qihao_97@126.com>
2022-08-16 23:56:29 +0800

    带有私有成员变量的学生类，要用set和get方法来设置和获取成员变量

--- Stage01JavaSEBasic/src/section_02_oop/private_key/StudentPrivate.java
+++ Stage01JavaSEBasic/src/section_02_oop/private_key/StudentPrivate.java
@@ -4,6 +4,9 @@ package section_02_oop.private_key;
  * @Author: Qihao
  * @Time: 2022/2022/8/16/23:25
  * @Descriptions: 带private的Student类
+ *
+ * this关键字：
+ * 成员方法的形参与成员变量同名时，this限定的变量表示成员变量
  */
 public class StudentPrivate {
     private int age = 20;
