5272add774963635df62b52b41588d14ea19a6df
qihao <qihao_97@163.com>
2022-09-15 14:16:33 +0800

    To test Github plugin for VS code.

---
+++ Stage01JavaSEBasic/src/section_01_basic/HelloWorldGithubTest.java
@@ -0,0 +1,9 @@
+package section_01_basic;
+
+public class HelloWorldGithubTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Github!");
+        System.out.println("Hello VS Code!");
+    }
+}
