146f1de1da877de411c912c08d069cd5cdac811e
qihao <qihao_97@126.com>
2022-11-08 16:19:19 +0800

    修复Stage02模块

---
+++ Stage02JavaSEAdvance/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Qihao
+ * @Time: 2022/11/8/16:13
+ * @Descriptions:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
