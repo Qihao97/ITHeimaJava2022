6a56b10c70739de7831e0274bb6b3bcec788249f
qihao <qihao_97@163.com>
2023-08-29 17:56:51 +0800

    Java程序设计前两章源码

--- Stage01JavaSEBasic/src/section_01_basic/hello_world/HelloWorld.java
+++ Stage01JavaSEBasic/src/section_01_basic/hello_world/HelloWorld.java
@@ -3,7 +3,7 @@ package section_01_basic.hello_world;
 /**
  * @Author: Qihao
  * @Time: 2022/2022/8/16/22:56
- * @Descriptions: Hello World!
+ * @Descriptions:
  */
 public class HelloWorld {
     public static void main(String[] args) {
